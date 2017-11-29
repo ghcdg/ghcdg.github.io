efdf6bb4506bb4065173afbc70973a397e3015ce
ghcdg <31769706+ghcdg@users.noreply.github.com>
2017-11-29 15:46:22 +0800

    Add files via upload

---
+++ js/getCoordinate.js
@@ -0,0 +1,12 @@
+/**
+ * 该js文件用来获取页面中矩形的实时坐标
+ */
+	function cnvs_getCoordinates(e){
+		x=e.clientX;
+		y=e.clientY;
+		document.getElementById("xycoordinates").innerHTML="Coordinates: (" + x + "," + y + ")";
+	}
+	 
+	function cnvs_clearCoordinates(){
+		document.getElementById("xycoordinates").innerHTML="";
+	}
