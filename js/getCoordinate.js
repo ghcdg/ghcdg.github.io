b59bef03379327e4117c7bb17c9fe1a6963f1617
ghcdg <31769706+ghcdg@users.noreply.github.com>
2017-12-14 21:13:14 +0800

    Update getCoordinate.js

--- js/getCoordinate.js
+++ js/getCoordinate.js
@@ -10,3 +10,11 @@
 	function cnvs_clearCoordinates(){
 		document.getElementById("xycoordinates").innerHTML="";
 	}
+/*
+将此段代码复制到index.html页面即可查看效果
+<div id="coordiv" style=" margin:0 auto;width:300px;height:150px;border:1px solid #c3c3c3"   
+ onmousemove="cnvs_getCoordinates(event)" onmouseout="cnvs_clearCoordinates()">
+ <p>把鼠标悬停在下面的矩形上可以看到坐标：</p>
+</div>
+<div id="xycoordinates" ></div>
+*/
